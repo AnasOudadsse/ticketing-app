1474633971429cea5fe86d296931e6f3edcfdef3
Hakim2Mezrioui <mezrioui.hakim@gmail.com>
2024-10-09 12:11:30 +0100

    side bar ui

--- front-end/src/component/side-bar/side-bar.jsx
+++ front-end/src/component/side-bar/side-bar.jsx
@@ -1,8 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDashboard, faUsers, faTicket } from '@fortawesome/free-solid-svg-icons'
+import LinkSideBar from '../link-side-bar/link-side-bar';
+
 const SideBar = () => {
     return (
-        <div className="bg-slate-300 w-80 h-screen">
-            <div>
-                <img src="/assets/images/logo.jpg"  className={"w-20 h-20 grid grid-cols-3 gap-4 content-center"} />
+        <div className="bg-slate-100 w-60 h-screen">
+            <div className={"flex justify-center w-full h-40 grid grid-cols content-center"}>
+                <img src="/assets/images/logo.jpg"  className={"w-20 h-20 "} />
+            </div>
+            <div className='flex justify-center'>
+                <ul className='w-full'>
+                    <LinkSideBar title={"Dashboard"} />
+                    <LinkSideBar title={"Users"} />
+                    <LinkSideBar title={"Tickets"} />
+                </ul>
             </div>
         </div>
     )
