7d9b0cdfa2c2bdddd908ac2db5590dee8a7f9978
Anas Oudadsse <anas.oudadsse1@gmail.com>
2024-10-10 16:42:14 +0100

    "Added UserController and new API route for adding users"

---
+++ back-end/app/Http/Controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8',
+            'role' => 'required|in:admin,supportIt,client',
+            // 'fonction_id' => 'required|exists:fonctions,id',
+            // 'departement_id' => 'required|exists:departements,id',
+            // 'localisation_id' => 'required|exists:localisations,id',
+        ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password), 
+            'role' => $request->role,
+            'fonction_id' => $request->fonction_id,
+            'departement_id' => $request->departement_id,
+            'localisation_id' => $request->localisation_id,
+        ]);
+
+        
+        return response()->json([
+            'message' => 'User created successfully',
+            'user' => $user
+        ], 201);
+    }
+
+}
--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,8 @@ use App\Http\Controllers\TicketController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::post('/addUser', [UserController::class, 'store']);
 
 Route::prefix('tickets')->group(function () {
     Route::post('/create', [TicketController::class, 'store']);
