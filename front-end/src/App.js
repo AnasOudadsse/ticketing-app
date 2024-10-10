756af9909dd5749a122fc1cc0c712b0a76b0eb82
Hakim2Mezrioui <mezrioui.hakim@gmail.com>
2024-10-10 08:48:18 +0100

    ...

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,7 +4,6 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NewTicket } from "./component/add-ticket/add-ticket";
 import TicketList from "./component/tickets-list/tickets-list";
-import Statistiques from "./component/statistiques/statistiques";
 
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
           <Routes>
               <Route element={<NewTicket/>} path="newticket" />
               <Route element={<TicketList/>} path="ticketlist" />
-              <Route element={<Statistiques />} path="statistiques" />
           </Routes>
         </BrowserRouter>
     </ChakraProvider>
