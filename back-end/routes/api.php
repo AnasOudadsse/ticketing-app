f65d60fcab99b325d221495acaf43c5cdd8170eb
lehlalimhamed <lehlalimhamed@gmail.com>
2024-10-10 16:38:50 +0100

    pour la creation de ticket

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -21,7 +21,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::prefix('tickets')->group(function () {
-    Route::post('/create', [TicketController::class, 'store']);
+    Route::post('/', [TicketController::class, 'store']);
     Route::get('/unassigned', [TicketController::class, 'listUnassignedTickets']);
     Route::post('/{id}/reserve', [TicketController::class, 'reserveTicket']);
     Route::post('/{id}/assign', [TicketController::class, 'assignTicketByAdmin']);
