0271bff8e50a11c59e511cbe4f7cb0c812ed8f4c
Hakim2Mezrioui <mezrioui.hakim@gmail.com>
2024-10-15 09:12:08 +0100

    nothing

--- front-end/src/component/add-user/addUser.jsx
+++ front-end/src/component/add-user/addUser.jsx
@@ -1,7 +1,7 @@
 const AddUser = () => {
   return (
     <div className="w-full h-screen bg-slate-100">
-      <form className="bg-gray-500 w-1/2 m-auto p-5">
+      <form className=" w-1/2 m-auto p-5">
         <div>
           <label>Name</label>
           <input className="block w-full px-2 py-1 outline-none rounded-md" />
