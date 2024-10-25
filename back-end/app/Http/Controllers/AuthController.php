1a7fa10f008635361ef7d400fe6fd11f97e4d123
Hakim2Mezrioui <mezrioui.hakim@gmail.com>
2024-10-25 11:56:50 +0100

    try catch

--- back-end/app/Http/Controllers/AuthController.php
+++ back-end/app/Http/Controllers/AuthController.php
@@ -12,6 +12,7 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        try{
         $validatedData = $request->validate([
             'name' => 'required|string|max:255|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
