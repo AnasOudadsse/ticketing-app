aa97f60877d60365b206fc7b5cd461a5897b9a1a
Hakim2Mezrioui <mezrioui.hakim@gmail.com>
2024-11-05 09:43:50 +0100

    update AuthController.php, api.php, useHttp.jsx and updateUser.jsx

--- front-end/src/component/update-user/updateUser.jsx
+++ front-end/src/component/update-user/updateUser.jsx
@@ -3,18 +3,40 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import useHttp from "../customHook/useHttp";
 
 const UpdateUser = () => {
-  const [user, setUser] = useState("");
   const params = useParams();
   const navigate = useNavigate();
   const {sendRequest, loading} = useHttp();
-  const [departements, setDepartements] = useState();
+  
+  const [user, setUser] = useState({});
+  const [departements, setDepartements] = useState([]);
+  const [localisations, setLocalisations] = useState([]);
 
+  
   useEffect(() => {
     // setRoleUser(params.role);
     const id = params.id;
     const token = localStorage.getItem("accessToken");
 
-    const request = {
+    
+    const request1 = {
+      url: `http://127.0.0.1:8000/api/departements`,
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }
+
+    sendRequest(request1, getData1);
+
+    const request2 = {
+      url: `http://127.0.0.1:8000/api/localisations`,
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }
+
+    sendRequest(request2, getData2);
+
+    const request3 = {
       url: `http://127.0.0.1:8000/api/user/${id}`,
       method: "GET",
       headers: {
@@ -23,11 +45,31 @@ const UpdateUser = () => {
       },
     };
 
-    sendRequest(request, getData);
+    sendRequest(request3, getData3);
   }, []);
 
-  const getData = (dataRec) => {
+  const getData1 = (dataRec) => {
+    setDepartements([...dataRec]);
+  }
+  const getData2 = (dataRec) => {
+    setLocalisations(dataRec);
+  }
+  const getData3 = (dataRec) => {
     setUser(dataRec);
+  }
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
   }
 
   return (
@@ -37,7 +79,7 @@ const UpdateUser = () => {
         className="fixed top-0 left-0 z-10 h-screen w-full"
         style={{ backgroundColor: "rgba(0, 0, 0, .5)" }}
       ></div>
-      <form className="absolute top-20 left-1/4 z-40 rounded shadow py-10 mt-10  border-l-4 border-l-gray-600 w-1/2 p-5 bg-white">
+      <form onSubmit={handleSubmit} className="absolute top-20 left-1/4 z-40 rounded shadow py-10 mt-10  border-l-4 border-l-gray-600 w-1/2 p-5 bg-white">
         <img
           className="  h-28 mb-12"
           src="https://um6ss.ma/wp-content/uploads/2024/02/UM6SS.png"
@@ -45,11 +87,11 @@ const UpdateUser = () => {
         />
         <div className="flex justify-between gap-3 items-center my-3">
           <label className="w-32">Name</label>
-          <input value={user.name} className="block w-full px-2 py-1 outline-none rounded-md  border" />
+          <input onChange={handleChange} value={user.name || ''} className="block w-full px-2 py-1 outline-none rounded-md  border" />
         </div>
         <div className="flex justify-between gap-3 items-center my-3">
           <label className="w-32">Email</label>
-          <input value={user.email} className="block w-full px-2 py-1 outline-none rounded-md  border" />
+          <input onBlur={handleChange} value={user.email} className="block w-full px-2 py-1 outline-none rounded-md  border" />
         </div>
 
         <div className="flex justify-between gap-3 items-center my-3">
@@ -58,7 +100,7 @@ const UpdateUser = () => {
             required
             name="role"
             onChange={() => {}}
-            onBlur={(e) => {}}
+            onBlur={handleChange}
             className="rounded-md w-full px-2 py-2 bg-white border"
           >
             <option selected disabled value={null}>
@@ -104,13 +146,12 @@ const UpdateUser = () => {
 
         <div className="flex justify-between gap-3 items-center my-3">
           <label className="w-32">Departement</label>
-          <select className="rounded-md w-full px-2 py-2 bg-white border">
+          <select onChange={handleChange} className="rounded-md w-full px-2 py-2 bg-white border">
             <option selected value={null} disabled>Select a Departement</option>
             {
-              departements.map((departement) => {
-
-                <option value={departement.id}>{departement.name}</option>
-              })
+              departements.map((departement) => (
+                <option selected={departement.id === user.departement_id} value={departement.id}>{departement.name}</option>
+              ))
             }
             {/* <option value={"Canvas"}>Canvas</option>
             <option value={"Biostar"}>Biostar</option> */}
@@ -119,16 +160,17 @@ const UpdateUser = () => {
 
         <div className="flex justify-between gap-3 items-center my-3">
           <label className="w-32">Localisation</label>
-          <select className="rounded-md w-full px-2 py-2 bg-white border">
-            <option disabled>Select a Localisation</option>
-            <option value={"Anfa"}>Anfa</option>
-            <option value={"Ligue arab"}>Ligue arab</option>
-            <option value={"anas inboMail"}>anas inboMail</option>
+          <select onChange={handleChange} className="rounded-md w-full px-2 py-2 bg-white border">
+            <option value={null} disabled>Select a Localisation</option>
+            {localisations.map((localisation) => (
+              <option selected={localisation.id === user.localisation_id} value={localisation.id}>{localisation.name}</option>
+            ))
+          }
           </select>
         </div>
         <div className="flex gap-2 mt-10">
           <button className="bg-green-800 rounded py-2 px-16 text-white hover:bg-green-700">
-            Ajouter
+            Update
           </button>
           <Link
             to={"/tickets/usersList"}
